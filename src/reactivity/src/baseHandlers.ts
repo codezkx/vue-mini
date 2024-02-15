28e417c5c7ac06af51ecbd5db28cadebb63a9ba5
codeZ <820025529@qq.com>
2024-02-15 19:39:34 +0800

    feat: readonly实现

---
+++ src/reactivity/src/baseHandlers.ts
@@ -0,0 +1,46 @@
+import { extend } from "../../shared";
+import { track, trigger } from "../src/effect";
+
+/* 
+  优化点 当get执行时不需要每次调用createGetter或者createSetter，所以只需要模块引用时执行一次即可(利用缓存技术)
+*/
+const get = createGetter(false);
+const set = createSetter();
+
+const readonlyGet = createGetter(true);
+
+export const mutableHandler = { get, set };
+
+function createGetter(isReadonly) {
+  return function (target, key) {
+    // 1、获取目标对象的属性值
+    const res = Reflect.get(target, key);
+    // 2、收集依赖
+    if (!isReadonly) {
+      track(target, key);
+    }
+    // 3、返回对应的数据
+    return res;
+  };
+}
+
+function createSetter() {
+  return function (target, key, value) {
+    // 1、设置对应的属性
+    const res = Reflect.set(target, key, value);
+    // 2、触发依赖更新
+    trigger(target, key);
+    // 3、返回是否设置成功
+    return res;
+  };
+}
+
+export const readonlyHandlers = {
+  get: readonlyGet,
+  set: function (target, key, value) {
+    console.warn(
+      `Set operation on key "${String(key)}" failed: target is readonly.`
+    );
+    return true;
+  },
+};
--- src/reactivity/src/reactive.ts
+++ src/reactivity/src/reactive.ts
@@ -1,37 +1,13 @@
-import { track, trigger } from "../src/effect";
+import { mutableHandler, readonlyHandlers } from "./baseHandlers";
 
 export function reactive(row) {
-  return new Proxy(row, {
-    get(target, key) {
-      // 1、获取目标对象的属性值
-      const res = Reflect.get(target, key);
-      // 2、收集依赖
-      track(target, key);
-      // 3、返回对应的数据
-      return res;
-    },
-    set(target, key, value): any {
-      // 1、设置对应的属性
-      const res = Reflect.set(target, key, value);
-      // 2、触发依赖更新
-      trigger(target, key);
-      // 3、返回是否设置成功
-      return res;
-    },
-  });
+  return createActiveObject(row, mutableHandler);
 }
 
 export function readonly(row) {
-  return new Proxy(row, {
-    get: function (target, key) {
-      const res = Reflect.get(target, key);
-      return res;
-    },
-    set: function (target, key, value) {
-      console.warn(
-        `Set operation on key "${String(key)}" failed: target is readonly.`
-      );
-      return true;
-    },
-  });
+  return createActiveObject(row, readonlyHandlers);
+}
+
+function createActiveObject(raw, baseHandlers) {
+  return new Proxy(raw, baseHandlers);
 }
